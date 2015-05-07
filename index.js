0641ecac2def2c8da479c377d8dc17a091df24bf
John Otander <johnotander@gmail.com>
2015-05-06 22:58:19 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = function hasAttributeSelector(options) {
+  options = options || {};
+
+  return true;
+};
---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var assert = require('assert');
+var hasAttributeSelector = require('..');
+
+describe('has-attribute-selector', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(hasAttributeSelector(), true);
+  });
+});
